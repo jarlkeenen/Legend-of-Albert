6af4a252c7e0ce11a199c6562152c72529415995
Henchelle Rae Omecillo <omecillo9152@users.noreply.github.com>
2020-11-13 15:27:35 +0800

    Screen is for window screen

---
+++ src/com/group4/HealthBar.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HealthBar extends Screen{
+    JPanel HPBar, Buttonpanel;
+    JProgressBar HealthBar;
+    JButton attack,heal;
+    //current player hp
+    private int hp;
+
+    public void playerHPBAR(int maxhp){
+
+        HPBar = new JPanel();
+        HPBar.setBounds(250, 250, 300, 30);
+        HPBar.setBackground(Color.BLACK);
+        con.add(HPBar);
+
+        HealthBar = new JProgressBar(0,maxhp);
+        HealthBar.setPreferredSize(new Dimension(300, 30));
+        HealthBar.setValue(100);
+        HealthBar.setBackground(Color.RED);
+        HealthBar.setForeground(Color.GREEN);
+        HPBar.add(HealthBar);
+
+        window.setVisible(true);
+
+    }
+
+    public void DamageReceived(int dmg) {
+        hp = hp - dmg;
+        HealthBar.setValue(hp);
+    }
+    public void HealReceived(int heal){
+        hp=hp+heal;
+        HealthBar.setValue(hp);
+
+    }
+
+}
